ceb99d74979590297b002bf5f5833afada7743fd
KhadorCrommanda <133072477+KhadorCrommanda@users.noreply.github.com>
2023-05-11 21:25:18 -0300

    Repaginamento das Funções e novas adições.
    Foi adicionado a Função excluir_conta(), feita para que usuários excluíssem suas contas. 
    Agora tanto a função login() quanto cadastrar_usuarios() possuem o laço de repetição While, em vez de utilizar chamada recursiva caso um dado fosse inserido incorretamente. (Isso poderia causar um Estouro de Pilha (Stack Overflow) caso muitos usuários inserissem seus dados incorretamente.

--- Sistema_de_cadastramento_e_login.py
+++ Sistema_de_cadastramento_e_login.py
@@ -10,59 +10,78 @@ import json
 # A função a seguir é responsável pela leitura das informações do arquivo JSON
 def carregar_usuarios():
     try:
-        with open('usuarios.json', 'r') as f:
+        with open('Usuarios.json', 'r') as f:
             return json.load(f)
     except FileNotFoundError:
         return {}
 
 # E essa função, pelo armazenamento das informações de login no arquivo JSON
-def salvar_usuarios(usuarios):
-    with open('usuarios.json', 'w') as f:
-        json.dump(usuarios, f)
-
-## Sistema principal
-Usuarios = carregar_usuarios() # Esse código anexa as informações do JSON na variável DICT "Usuarios"
+def salvar_usuarios(Usuarios):
+    with open('Usuarios.json', 'w') as f:
+        json.dump(Usuarios, f)
 
 # Essa é a função que abriga todo o código responsável pelo login.
 def login():
-    nome = input("Olá, informe seu nome de usuário: ")
-    if(nome in Usuarios.keys()):
-        senha = int(input(f"Bem-vindo de volta, {nome}! Informe sua senha: "))
-        # Se o nome que o usuário escrever for uma das chaves do dicionário 'Usuarios',
-        # será solicitada a senha (ou seja, o valor da chave).
-        if(senha == Usuarios[nome]["Senha"]):
-            print(f"Acesso permitido para o usuário '{nome}'.")
-            # Após o usuário escrever seu nome de usuário e sua senha, será exibida uma mensagem de finalização.
+    while True:
+        nome = input("Olá, informe seu nome de usuário: ")
+        if (nome in Usuarios):
+            senha = int(input(f"Bem-vindo de volta, {nome}! Informe sua senha: "))
+            if (senha == Usuarios[nome]["Senha"]):
+                print(f"Acesso permitido para o usuário '{nome}'.")
+                return # encerra a função login() com sucesso
+            else:
+                print("Senha incorreta, tente novamente.")
         else:
-            print("Senha incorreta, tente novamente.")
-            login() # Se o usuário digitar uma senha incorreta, será solicitada novamente.
-    else:
-        Usuarios[nome] = {"User": nome, "Senha": 1234} # Caso o nome de usuário não exista, é criado um novo usuário com senha padrão 1234.
-        print(f"Seu nome de usuário '{nome}' foi registrado com sucesso! Use a senha padrão '1234' para fazer o login.")
-        salvar_usuarios(Usuarios)
-        login()
+            print("Nome de usuário não encontrado.")
+            opcao = input("Digite '1' para tentar novamente ou '2' para cadastrar um novo usuário: ")
+            if opcao == "2":
+                cadastrar_usuario()
+            elif opcao != "1":
+                print("Opção inválida, encerrando o programa.")
+                return # encerra a função login() com falha
 
 def cadastrar_usuario():
-    nome = input("Informe um nome de usuário: ")
-    if(nome in Usuarios.keys()):
-        print("Este nome de usuário já existe, tente outro.")
-        cadastrar_usuario() # Caso o nome de usuário já exista, é solicitado que o usuário informe outro nome.
-    else:
-        try:
-            senha = int(input("Informe uma senha: "))
-        except:
-            print("Ops, erro 0x1. Provavelmente sua senha contém letras (somente números são permitidos).")
-            cadastrar_usuario() # Caso a senha contenha letras em vez de números, é solicitado que o usuário informe outra senha.
+    while True:
+        nome = input("Informe um nome de usuário: ")
+        if(nome in Usuarios.keys()):
+            print("Este nome de usuário já existe, tente outro.")
+            # Caso o nome de usuário já exista, é solicitado que o usuário informe outro nome.
         else:
-            Usuarios[nome] = {"User": nome, "Senha": senha} # Se o nome de usuário escolhido e a senha informada forem válidos, o novo usuário é cadastrado.
-            print(f"Usuário '{nome}' cadastrado com sucesso!")
+            try:
+                senha = int(input("Informe uma senha: "))
+            except:
+                print("Ops, erro 0x1. Provavelmente sua senha contém letras (somente números são permitidos).")
+                # Caso a senha contenha letras em vez de números, é solicitado que o usuário informe outra senha.
+            else:
+                Usuarios[nome] = {"User": nome, "Senha": senha} # Se o nome de usuário escolhido e a senha informada forem válidos, o novo usuário é cadastrado.
+                print(f"Usuário '{nome}' cadastrado com sucesso!")
+                salvar_usuarios(Usuarios)
+                return
+
+def excluir_conta():
+    nome = input("Informe seu nome de usuario... ")
+    if(nome in Usuarios.keys()):
+        senha = int(input("Informe a senha de sua conta... "))
+        if(senha == Usuarios[nome]["Senha"]):
+            print("Excluindo conta do sistema...\n")
+            del Usuarios[nome]            
+            print("Conta Excluida com sucesso.")
             salvar_usuarios(Usuarios)
+        else:
+            print("Senha incorreta, tente novamente.")
+            excluir_conta()
+    else:
+        print("Usuario inexistente.")
+        excluir_conta();
 
 # Programa principal
-opcao = input("Digite '1' para fazer login ou '2' para cadastrar um novo usuário: ")
+Usuarios = carregar_usuarios() # Esse código anexa as informações do JSON na variável DICT "Usuarios"
+opcao = input("Digite '1' para fazer login, '2' para cadastrar um novo usuário ou '3' para excluir sua conta: ")
 if(opcao == "1"):
     login()
 elif(opcao == "2"):
     cadastrar_usuario()
+elif(opcao == "3"):
+    excluir_conta()
 else:
     print("Opção invalida")
